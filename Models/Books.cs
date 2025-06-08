45a13ad8aae4bd29d6c4a3ade922aea00f6cf488
swedenebomi <swedenebomi@gamil.com>
2025-06-08 21:33:13 +0200

    Book model to support ObjectId from MongoDB

--- Models/Books.cs
+++ Models/Books.cs
@@ -5,16 +5,29 @@ namespace dotnet_backend.Models
 {
     public class Book
     {
-        [BsonId] // Used as the _id field in MongoDB
-        [BsonRepresentation(BsonType.ObjectId)] // MongoDB handles ObjectId, C# handles it as a string
+        [BsonId] // 이 속성은 MongoDB의 _id 필드로 사용됨
+        [BsonRepresentation(BsonType.ObjectId)] // C#에선 string, MongoDB에선 ObjectId로 저장
         public string? Id { get; set; }
 
+        [BsonElement("title")]
         public string Title { get; set; } = "";
+
+        [BsonElement("author")]
         public string Author { get; set; } = "";
+
+        [BsonElement("publishedDate")]
         public string PublishedDate { get; set; } = "";
+
+        [BsonElement("pages")]
         public int Pages { get; set; }
+
+        [BsonElement("description")]
         public string Description { get; set; } = "";
+
+        [BsonElement("genre")]
         public string Genre { get; set; } = "";
+
+        [BsonElement("imageUrl")]
         public string ImageUrl { get; set; } = "";
     }
 }
